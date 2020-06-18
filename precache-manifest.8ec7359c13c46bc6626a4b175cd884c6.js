747bdf4c7085b7ec5617e8c726c293683ec5f307
Selynna Sun <ssun098@gmail.com>
2020-06-18 16:48:46 -0700

    Updates

---
+++ precache-manifest.8ec7359c13c46bc6626a4b175cd884c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa83819d1e38955ee8289082eaf49347",
+    "url": "/revamp/index.html"
+  },
+  {
+    "revision": "f348d2ef16be2090a12c",
+    "url": "/revamp/static/css/main.8530ac47.chunk.css"
+  },
+  {
+    "revision": "43bfb5ff61a48b3a0f8f",
+    "url": "/revamp/static/js/2.367f1fb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/revamp/static/js/2.367f1fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f348d2ef16be2090a12c",
+    "url": "/revamp/static/js/main.0d84bbca.chunk.js"
+  },
+  {
+    "revision": "061e9010ca3828a1af10",
+    "url": "/revamp/static/js/runtime-main.9c447a0a.js"
+  },
+  {
+    "revision": "b365616d1f4d475d3206006ba0701a39",
+    "url": "/revamp/static/media/proximanova-bold.b365616d.ttf"
+  },
+  {
+    "revision": "a61e6095a651a5afbbfc098adb5fa8a8",
+    "url": "/revamp/static/media/proximanova-regular.a61e6095.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5022fb7310ed878e34dc7ac7fcc83d13.js"
+  "/revamp/precache-manifest.8ec7359c13c46bc6626a4b175cd884c6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/revamp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
